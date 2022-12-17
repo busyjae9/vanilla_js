49d652d0d43519b7d0d733fcf45d9691c2fca8e3
JAE HUN <kisdelov@gmail.com>
2022-12-18 01:22:32 +0900

    style: mobile style apply

---
+++ todos/knex_migrations_home/20221217141834_create_archive.cjs
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("archive", table => {
+    table.bigInteger("todo_id").unsigned();
+    table.foreign("todo_id").references("todos.id");
+
+    table.datetime("deleted_date");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+
+};
